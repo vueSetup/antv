07db16d9302e048073dc30cc5c35cf7b1563ccdc
KE.YANG <ke.yang@outlook.com>
2022-07-19 00:07:20 +0800

    feat(@antv/x6-vue-components): toolbar

---
+++ packages/x6-vue-components/src/toolbar/context.ts
@@ -0,0 +1,16 @@
+import type { InjectionKey } from 'vue'
+import { createContext, useContext } from '../composables'
+
+export interface IToolbarContext {
+    prefixCls?: string
+    onClick: (key: string, value?: any) => void
+}
+
+const contextKey: InjectionKey<IToolbarContext> = Symbol('Toolbar')
+
+export const ToolbarContextProvider = createContext<IToolbarContext>(
+    contextKey,
+    'ToolbarContextProvider'
+)
+
+export const useToolbarContext = () => useContext<IToolbarContext>(contextKey, {})
---
+++ packages/x6-vue-components/src/toolbar/group.tsx
@@ -0,0 +1,16 @@
+import { defineComponent, computed } from 'vue'
+import { useToolbarContext } from './context'
+
+export const ToolbarGroup = defineComponent({
+    setup(props, { slots }) {
+        const context = useToolbarContext()
+
+        return () => {
+            const baseClassName = [`${context.prefixCls}-group`]
+
+            const children = slots.default?.()
+
+            return <div class={baseClassName}>{children}</div>
+        }
+    }
+})
