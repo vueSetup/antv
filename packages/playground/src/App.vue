3183d7fa82cd8e35cb326253763aed3711fe356b
KE.YANG <ke.yang@outlook.com>
2022-07-23 00:38:03 +0800

    style(playground): style

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,38 +1,39 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import { ref } from 'vue'
-import { F2Vue, F2CanvasGraphic } from './views'
+import { ref } from 'vue';
+import {
+  X6Menu,
+  X6Menubar,
+  X6Dropdown,
+  X6Toolbar,
+  X6ContextMenu,
+  F2Vue,
+  F2CanvasGraphic,
+} from './views';
 
-const count = ref(0)
-const prefixCls = ref('x6')
+const count = ref(0);
+const prefixCls = ref('x6');
 
 const onClick = () => {
-    count.value += 1
-    prefixCls.value = prefixCls.value === 'x6' ? 'antdv' : 'x6'
-}
+  count.value += 1;
+  prefixCls.value = prefixCls.value === 'x6' ? 'antdv' : 'x6';
+};
 </script>
 
 <template>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
-    </div>
-    <button @click="onClick">{{ count }}</button>
-    <F2Vue />
-    <!-- <F2CanvasGraphic/> -->
-    <!-- <f2-line-compare /> -->
-    <!-- <f2-line-anchor /> -->
-    <!-- <f2-line-connect-null /> -->
-    <!-- <f2-gauge /> -->
-    <!-- <f2-axis-grid /> -->
-    <!-- <f2-pie-select /> -->
-    <!-- <f2-guide-text /> -->
-    <!-- <x6-menu />
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <button @click="onClick">{{ count }}</button>
+  <F2Vue />
+  <!-- <F2CanvasGraphic /> -->
+  <!-- <x6-menu />
     <x6-dropdown />
     <x6-toolbar />
     <x6-context-menu />
@@ -41,14 +42,14 @@ const onClick = () => {
 
 <style scoped>
 .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
 }
 .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,53 +1,49 @@
-import { loadEnv, searchForWorkspaceRoot, type ConfigEnv, type UserConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import { join } from 'path'
-
-const shape = ['rect', 'line', 'text', 'circle', 'marker', 'group']
+import { loadEnv, searchForWorkspaceRoot, type ConfigEnv, type UserConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import { join } from 'path';
 
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv): UserConfig => {
-  const env = loadEnv(mode, process.cwd())
+  const env = loadEnv(mode, process.cwd());
 
   return {
     base: env.VITE_BASE_URL,
     define: {
-      'process.env.BASE_URL': JSON.stringify(env.VITE_BASE_URL)
+      'process.env.BASE_URL': JSON.stringify(env.VITE_BASE_URL),
     },
     optimizeDeps: {
-      exclude: ['vue-demi']
+      exclude: ['vue-demi'],
     },
     plugins: [
       vue(),
       vueJsx({
-        isCustomElement: (tagName: string) =>
-          ['group', 'rect', 'circle', 'line', 'polygon', 'polyline', 'arc', 'sector', 'text', 'custom', 'marker', 'image'].includes(tagName)
-      })],
+        isCustomElement: (tagName: string) => ['group'].includes(tagName),
+      }),
+    ],
     resolve: {
       alias: {
-        '@': join(__dirname, './src')
-      }
+        '@': join(__dirname, './src'),
+      },
     },
     css: {
       preprocessorOptions: {
         less: {
           javascriptEnabled: true,
-        }
-      }
+        },
+      },
     },
     server: {
       fs: {
-        allow: [
-          searchForWorkspaceRoot(process.cwd())
-        ]
+        allow: [searchForWorkspaceRoot(process.cwd())],
       },
       proxy: {
         '/api': {
           target: 'http://11.11.160.192:48810',
           changeOrigin: true,
-          rewrite: path => path.replace(/^\/api/, '')
+          rewrite: (path) => path.replace(/^\/api/, ''),
         },
-      }
-    }
-  }
-}
+      },
+    },
+  };
+};
