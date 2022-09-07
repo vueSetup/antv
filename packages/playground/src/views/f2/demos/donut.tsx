ae916656f3654558d0889cf1bbfc792749fea6b2
buzzlightyear <794758159@qq.com>
2022-09-07 12:41:16 +0800

    fix(f2-vue): utils

---
+++ packages/playground/src/views/f2/demos/donut.tsx
@@ -0,0 +1,61 @@
+import { defineComponent } from 'vue'
+import { Canvas, Chart, Interval, Legend } from '@antv/f2-vue'
+
+const data = [
+    {
+        name: '股票类',
+        percent: 83.59,
+        a: '1'
+    },
+    {
+        name: '债券类',
+        percent: 2.17,
+        a: '1'
+    },
+    {
+        name: '现金类',
+        percent: 14.24,
+        a: '1'
+    }
+]
+
+export default defineComponent({
+    setup() {
+        return () => (
+            <Canvas theme={{ padding: [20, 'auto'] }}>
+                <Chart
+                    scale={{
+                        percent: {
+                            formatter: function formatter(val) {
+                                return val + '%'
+                            }
+                        }
+                    }}
+                    data={data}
+                    coord={{
+                        type: 'polar',
+                        transposed: true,
+                        innerRadius: 0.7,
+                        radius: 0.85
+                    }}
+                >
+                    <Interval
+                        x="a"
+                        y="percent"
+                        adjust="stack"
+                        color={{
+                            field: 'name',
+                            range: ['#FE5D4D', '#3BA4FF', '#737DDE']
+                        }}
+                    />
+                    <Legend
+                        position="right"
+                        itemFormatter={(val) => {
+                            return val + '    ' + map[val]
+                        }}
+                    />
+                </Chart>
+            </Canvas>
+        )
+    }
+})
---
+++ packages/playground/src/views/f2/demos/multiple.tsx
@@ -0,0 +1,45 @@
+import { defineComponent } from 'vue'
+import { Canvas, Chart, Interval } from '@antv/f2-vue'
+
+const data = [
+    { a: '1', genre: 'Sports', sold: 275 },
+    { a: '1', genre: 'Strategy', sold: 115 },
+    { a: '1', genre: 'Action', sold: 120 },
+    { a: '1', genre: 'Shooter', sold: 350 },
+    { a: '1', genre: 'Other', sold: 110 }
+]
+// issue:test时没有选中状态，demo可以，可能test未成功触发点击事件
+export default defineComponent({
+    setup() {
+        return () => (
+            <Canvas pixelRatio={1} height={300}>
+                <Chart
+                    data={data}
+                    coord={{
+                        radius: 0.8,
+                        type: 'polar',
+                        transposed: true
+                    }}
+                >
+                    <Interval
+                        x="a"
+                        y="sold"
+                        adjust="stack"
+                        color="genre"
+                        selection={{
+                            defaultSelected: [{ a: '1', genre: 'Strategy', sold: 115 }],
+                            selectedStyle: (record) => {
+                                const { yMax, yMin } = record
+                                return {
+                                    r: (yMax - yMin) * 1.1
+                                }
+                            },
+                            unSelectedStyle: {},
+                            cancelable: true
+                        }}
+                    />
+                </Chart>
+            </Canvas>
+        )
+    }
+})
